fa9fc0b04594196077631f43231b62b5115a9687
yeferic <yeferic@gmail.com>
2022-07-26 08:27:57 -0500

    Create splash screen

--- app/src/main/java/com/yeferic/pragmacats/SplashScreen.kt
+++ app/src/main/java/com/yeferic/pragmacats/SplashScreen.kt
@@ -3,9 +3,9 @@ package com.yeferic.pragmacats
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.screen_splash)
     }
 }
