0736c8ae005a5f9167744dd69fdc1c439872c19a
yeferic <yeferic@gmail.com>
2022-07-27 06:52:00 -0500

    Create splash screen and launcher icon

--- app/src/main/java/com/yeferic/pragmacats/SplashScreen.kt
+++ app/src/main/java/com/yeferic/pragmacats/SplashScreen.kt
@@ -1,11 +1,27 @@
 package com.yeferic.pragmacats
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.yeferic.pragmacats.presentation.catbreeds.screens.CatBreedsView
+import kotlinx.coroutines.*
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.screen_splash)
+
+        scope.launch {
+            delay(3000)
+            val intent = Intent(this@SplashScreen, CatBreedsView::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/yeferic/pragmacats/presentation/catbreeds/screens/CatBreedsView.kt
@@ -0,0 +1,12 @@
+package com.yeferic.pragmacats.presentation.catbreeds.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yeferic.pragmacats.R
+
+class CatBreedsView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_cat_breeds)
+    }
+}
