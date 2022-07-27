eebbf70f2bdadfe1d24f6e09ed823dac65e43c29
yeferic <yeferic@gmail.com>
2022-07-27 07:18:25 -0500

    Create constans and data classes

---
+++ app/src/main/java/com/yeferic/pragmacats/domain/model/Breed.kt
@@ -0,0 +1,9 @@
+package com.yeferic.pragmacats.domain.model
+
+data class Breed(
+    var name: String,
+    var intelligence: Int,
+    var image: Image,
+    var description: String,
+    var origin: String
+)
---
+++ app/src/main/java/com/yeferic/pragmacats/domain/model/Image.kt
@@ -0,0 +1,8 @@
+package com.yeferic.pragmacats.domain.model
+
+data class Image(
+    var id: String,
+    var width: Int,
+    var height: Int,
+    var url: String
+)
